003b99dfd3660532efe24756b83806eafbac5191
PercyHoquee <percyhoquee@gmail.com>
2021-12-08 00:10:23 +0100

    pitstop

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,33 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.fields import CharField, IntegerField
+from django.db.models.fields.related import ForeignKey
+from django.db.models import Max
 
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    title = CharField(max_length=100)
+    description = CharField(max_length=200)
+    start_price = IntegerField()
+    owner_id = ForeignKey(User, on_delete=models.CASCADE)
+    highest_bid = IntegerField(default=0)
+
+
+class Bid(models.Model):
+    amount = IntegerField()
+    bidder_id = ForeignKey(User, on_delete=models.CASCADE)
+    listing_id = ForeignKey(Listing, on_delete=models.CASCADE)
+
+    def maximum_bid(self, id):
+        return Bid.objects.filter(listing_id=id).aggregate(Max('amount'))
+
+
+class Comment(models.Model):
+    text = CharField(max_length=200)
+    commenter_id = ForeignKey(User, on_delete=models.CASCADE)
+    listing_id = ForeignKey(Listing, on_delete=models.CASCADE)
+
--- auctions/views.py
+++ auctions/views.py
@@ -1,14 +1,30 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
+from django.db.models.fields import CharField, IntegerField
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
 
-from .models import User
+from .models import Bid, User, Listing
+
+
+class NewListingForm(forms.Form):
+    title = forms.CharField(max_length=100)
+    description = forms.CharField(max_length=200)
+    start_price = forms.IntegerField(initial=0)
+
+
+class NewBidForm(forms.Form):
+    amount = forms.IntegerField()
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    listings = Listing.objects.all()
+
+    return render(request, "auctions/index.html", {
+        "listings": listings
+    })
 
 
 def login_view(request):
@@ -61,3 +77,67 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+def create(request):
+    if request.method == "POST":
+        form = NewListingForm(request.POST)
+
+        if form.is_valid():
+            title = request.POST["title"]
+            description = request.POST["description"]
+            start_price = request.POST["start_price"]
+
+            listing = Listing(title=title, description=description, start_price=start_price, owner_id=request.user, highest_bid=0)
+            listing.save()
+
+            return HttpResponseRedirect(reverse("index"))
+        else: 
+            return render(request, "auctions/create.html", {
+                "form": form
+            })
+    else:
+        return render(request, "auctions/create.html", {
+            "form": NewListingForm()
+        })
+
+
+def listing(request, listing_id):
+    if request.method == "POST":
+        form = NewBidForm(request.POST)
+
+        listing = Listing.objects.get(id=listing_id)
+
+        if form.is_valid():
+            amount = int(request.POST["amount"])
+
+            start_price = listing.start_price
+            highest_bid = listing.highest_bid
+
+            if amount >= start_price and amount >= highest_bid:
+                listing.highest_bid = amount
+                listing.save()
+
+                bid = Bid(amount=amount, bidder_id=request.user, listing_id=listing)
+                bid.save()
+
+                return HttpResponseRedirect(reverse("listing", args=[listing.id]))
+            else: 
+                return render(request, "auctions/listing.html", {
+                "listing": listing,
+                "form": NewBidForm(request.POST),
+                "message": "Bid Higher Please"
+            })
+        else:
+            return render(request, "auctions/listing.html", {
+                "listing": listing,
+                "form": NewBidForm(request.POST),
+            })
+    else:
+        listing = Listing.objects.get(id=listing_id)
+
+        return render(request, "auctions/listing.html", {
+            "listing": listing,
+            "form": NewBidForm()
+        })
+
